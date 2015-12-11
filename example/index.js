31139a7916ba250b0d176160af871103b76b6704
Cezary Daniel Nowak <cezary.nowak@gmail.com>
2015-12-11 19:08:59 +0100

    open added

--- example/index.js
+++ example/index.js
@@ -1,33 +1,35 @@
 var express = require('express')
 var APICacheProxy = require('node-api-cache-proxy')
 var fs = require('fs')
+var open = require('open')
 
 /*
  * configure cache proxy
  */
 var app = express()
 var apiCacheProxy = new APICacheProxy({
-    apiUrl: 'http://api.giphy.com/',
-    cacheDir: 'cache-api/',
-    localURLReplace: function(url) {
-        return url.replace('/giphy/', '/')
-    }
+  apiUrl: 'http://api.giphy.com/',
+  cacheDir: 'cache-api/',
+  localURLReplace: function(url) {
+    return url.replace('/giphy/', '/')
+  }
 })
 
 /*
  * start express app
  */
 app.listen(9999, 'localhost', function () {
-  console.log('Server started at ', 'localhost:9999')
+  console.log('Server started at ', 'http://localhost:9999')
+  open('http://localhost:9999')
 })
 
 /*
  * allow requests from any server
  */
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  return next()
+})
 
 /*
  * proxy all /giphy/* requests with localhost:9999
@@ -38,5 +40,5 @@ app.use('/giphy', apiCacheProxy)
  * demo page
  */
 app.use('/', function(req, res) {
-	fs.createReadStream('./index.html').pipe(res)
+  fs.createReadStream('./index.html').pipe(res)
 })
